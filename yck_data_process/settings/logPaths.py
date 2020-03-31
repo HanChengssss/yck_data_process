d52f8f9e3a1f8b686ab89492a5dfeedcd0a0a3db
Twilight <Twilight@qq.ocm>
2020-03-31 13:52:34 +0800

    add log_dir_test

--- yck_data_process/settings/logPaths.py
+++ yck_data_process/settings/logPaths.py
@@ -1,7 +1,25 @@
-testLogPathDic = {
+import glob
+import os
 
+logDirName = {
+    "normalLog": "log_dir"
 }
 
-normalLogPathDic = {
+projectBasePath = {
 
 }
+# ../*.py
+# ../log_dir
+# print(glob.glob(r'*.py'))
+
+def findfile(start, name):
+    # print(os.walk(start))
+    for relpath, dirs, files in os.walk(start):
+        # print(relpath, dirs, files)
+        if name in dirs:
+            print(relpath)
+            full_path = os.path.join(start, relpath, name)
+            print(os.path.normpath(os.path.abspath(full_path)))
+
+if __name__ == '__main__':
+    findfile(start="D:\YCK\代码\yck_data_process\yck_data_process", name="log_dir")
