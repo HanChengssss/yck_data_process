3b97044c8ac9121e611a44731dc5e8055e6bf9b4
Twilight <Twilight@qq.ocm>
2020-07-21 14:54:12 +0800

    修复银行数据bank_code为null

--- yck_data_process/process/bankInfoProcess.py
+++ yck_data_process/process/bankInfoProcess.py
@@ -11,5 +11,19 @@ class BankInfoProcessManage():
                 data = d["data"]
             else:
                 data = d
-            data["isManual"] = "false"
-            data["isSync"] = "false"
+            bank_code = data.get("bank_code")
+            if bank_code is None:
+                pass
+            else:
+                bank_code = bank_code.strip()
+            if not bank_code:
+               data["bank_code"] = "000000000000"
+               data["bank_type"] = "000000000000"
+               data["bank_province"] = "000000000000"
+               data["bank_name"] = "000000000000"
+               data["bank_city"] = "000000000000"
+               data["isManual"] = "false"
+               data["isSync"] = "false"
+            else:
+                data["isManual"] = "false"
+                data["isSync"] = "false"
